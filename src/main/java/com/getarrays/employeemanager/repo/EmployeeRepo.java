f63017866fe3dc3b141a21965ff7e4fdad391372
mahatma kawa <mahatmakawa@gmail.com>
2022-10-09 15:02:00 +0700

    Service class

--- src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/com/getarrays/employeemanager/repo/EmployeeRepo.java
@@ -3,6 +3,15 @@ package com.getarrays.employeemanager.repo;
 import com.getarrays.employeemanager.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepo extends JpaRepository<Employee, Long> { // param is class and id type
+import java.util.Optional;
 
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+
+    // this called query method, spring will understand what we want
+    void deleteEmployeeById(Long id); // param is class and id type
+
+    // this called query method, spring will understand what we want
+    // and because this is optional, sometimes the id given not present
+    // we add optional
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/getarrays/employeemanager/service/EmployeeService.java
+++ src/main/java/com/getarrays/employeemanager/service/EmployeeService.java
@@ -5,6 +5,8 @@ import com.getarrays.employeemanager.repo.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +21,24 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString()); // this will generate random UUID
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        // because its optional, we make else throw
+        return employeeRepo.findEmployeeById(id).orElseThrow(()->
+                new UserPrincipalNotFoundException(
+                        "User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
     }
 }
